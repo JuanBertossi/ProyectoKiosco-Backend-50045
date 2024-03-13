2d4d878bee6d5f8711cf6e212e1c4f2ff979fbd9
Juan Bertossi <bertossijuani@gmail.com>
2024-03-13 12:07:12 -0300

    Intendando Desafio

--- src/app.js
+++ src/app.js
@@ -1,31 +1,49 @@
 const express = require("express");
-const app = express();
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const exphbs = require("express-handlebars");
+const userRouter = require("./routes/user.routes.js");
+const sessionRouter = require("./routes/session.routes.js");
+const viewsRouter = require("./routes/views.routes.js");
+const initializePassport = require("./config/passport.config.js");
+const passport = require("passport");
+const app = express();
 const PUERTO = 8080;
 require("./database.js");
 
-const productsRouter = require("./routes/products.router.js");
-const cartsRouter = require("./routes/carts.routes.js");
-const viewsRouter = require("./routes/views.routes.js");
-
-//Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("./src/public"));
-
-//Handlebars
+//Express-Handlebar
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
 
-//Rutas:
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
+//Middleware
+app.use(express.static("./src/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secretCoder",
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://bertossijuani:Juanito2023@cluster0.ff4gbbu.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
+      ttl: 100,
+    }),
+  })
+);
+
+//Passport:
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/users", userRouter);
+app.use("/api/sessions", sessionRouter);
 app.use("/", viewsRouter);
-app.get("/", (req, res) => {
-  res.render("products");
-});
 
 app.listen(PUERTO, () => {
-  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+  console.log(`Escuchando en el puerto: ${PUERTO}`);
 });
